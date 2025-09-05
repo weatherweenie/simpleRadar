85266cd92c84cabe029929be1b1bfa84be0d0925
username <test@email.com>
2025-09-05 19:04:33 -0400

    station page is done, and region page is now just one html page with a url parameter for the region!

---
+++ radar_maps/javascript/region.js
@@ -0,0 +1,45 @@
+console.log("hello!");
+
+const urlParams = new URLSearchParams(window.location.search);
+const region = urlParams.get('region');
+
+console.log(region);
+
+regionanchor = document.getElementById("regionanchor");
+regionspan = document.getElementById("regionspan");
+
+//lookup table for region ids, a bit tedious
+if (region == "pacnorthwest") { regionName = "Pacific Northwest";}
+if (region == "northrockies") { regionName = "North Rockies";}
+if (region == "uppermissvly") { regionName = "Upper Mississippi Valley";}
+if (region == "centgrlakes") { regionName = "Central Great Lakes";}
+if (region == "northeast") { regionName = "Northeast";}
+if (region == "pacsouthwest") { regionName = "Pacific Southwest";}
+if (region == "southrockies") { regionName = "Southern Rockies";}
+if (region == "southplains") { regionName = "Southern Plains";}
+if (region == "southmissvly") { regionName = "Southern Mississippi Valley";}
+if (region == "southeast") { regionName = "Southeast";}
+
+if (region == "alaska") { regionName = "Alaska";}
+if (region == "guam") { regionName = "Guam";}
+if (region == "hawaii") { regionName = "Hawaii";}
+if (region == "tjua") { regionName = "Puerto Rico";}
+console.log(regionName);
+
+if (['guam', 'hawaii', 'alaska', 'tjua'].includes(region)) { //if the region is any of these
+	conusanchor.href = "./conus/conus.html";
+	conusanchor.innerText = "CONUS";
+	conusspan.innerText = " | ";
+	regionspan.innerText = regionName;
+}
+
+else { //if it's any of the regions within conus
+	conusanchor.href = "./conus/conus.html";
+	conusanchor.innerText = "CONUS";
+	conusspan.innerText = " > ";
+	regionspan.innerText = regionName;
+}
+
+regionCapitalized = region.toUpperCase();
+
+document.getElementById("mainImg").src = "https://radar.weather.gov/ridge/standard/" + regionCapitalized + "_loop.gif";
---
+++ radar_maps/javascript/station.js
@@ -0,0 +1,36 @@
+console.log("hello!");
+
+const urlParams = new URLSearchParams(window.location.search);
+const region = urlParams.get('region');
+const station = urlParams.get('station');
+
+console.log(region + ", " + station);
+
+region1anchor = document.getElementById("region1anchor");
+region2anchor = document.getElementById("region2anchor");
+region1span = document.getElementById("region1span");
+region2span = document.getElementById("region2span");
+
+regionCapitalized = region[0].toUpperCase() + region.slice(1);
+stationCapitalized = station.toUpperCase();
+console.log(stationCapitalized);
+
+if (['guam', 'hawaii', 'alaska', 'tjua'].includes(region)) { //if the region is any of these
+	region1anchor.href = "./" + region + "/" + region + ".html";
+	region1anchor.innerText = regionCapitalized;
+	region1span.innerText = " > ";
+	stationspan.innerText = stationCapitalized;
+	
+}
+
+else { //if it's any of the regions within conus
+	region1anchor.href = "./conus/conus.html";
+	region1anchor.innerText = "CONUS";
+	region1span.innerText = " > ";
+	region2anchor.href = "./conus/regions/" + region + "/" + region + ".html";
+	region2anchor.innerText= regionCapitalized;
+	region2span.innerText = " > ";
+	stationspan.innerText = stationCapitalized;
+}
+
+document.getElementById("mainImg").src = "https://radar.weather.gov/ridge/standard/" + stationCapitalized + "_loop.gif";
